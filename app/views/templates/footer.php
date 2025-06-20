9dc5d4d8190fee1f3f47293053aac875a3f9beec
MBadr-school <mbadr@algomau.ca>
2025-06-20 16:48:50 +0000

    update the styling of footer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,13 +1,14 @@
-<footer class="footer">    
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Copyright &copy; <?php echo date('Y'); ?> </p>
-        </div>
-    </div>
+</main>
+<footer class="bg-light border-top py-3">
+  <div class="container text-center small text-muted">
+    &copy; <?= date('Y'); ?> COSC 4806
+  </div>
 </footer>
 
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-dgtcG0JlhD1Qo2yrFWFxi9nWChmdB+VbDoOItd9TRBTXK1347So7jRw4Y0xEhuPK"
+  crossorigin="anonymous">
+</script>
 </body>
 </html>
