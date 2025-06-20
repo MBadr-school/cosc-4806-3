f9c8f7cf7195fb686bf2107a764137560058e951
MBadr-school <mbadr@algomau.ca>
2025-06-20 16:50:11 +0000

    one more fix

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -5,10 +5,8 @@
   </div>
 </footer>
 
-<script
-  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-dgtcG0JlhD1Qo2yrFWFxi9nWChmdB+VbDoOItd9TRBTXK1347So7jRw4Y0xEhuPK"
-  crossorigin="anonymous">
-</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
