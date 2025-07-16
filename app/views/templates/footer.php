1254ca175590f09a281b936aa666e3f240849186
MBadr-school <mbadr@algomau.ca>
2025-07-16 17:23:32 +0000

    .

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -72,7 +72,7 @@
                 <div class="col-md-8">
                     <p class="text-muted mb-0">
                         &copy; <?= date('Y'); ?> ReminderApp. All rights reserved. 
-                        <span class="d-none d-md-inline">Built by Badr with</span>
+                        <span class="d-none d-md-inline">Built by Badr</span>
                     </p>
                 </div>
                 <div class="col-md-4 text-md-end">
